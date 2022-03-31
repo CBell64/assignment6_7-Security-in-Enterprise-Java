c0373249f8889b11a6fb42c64b0c99f699ec7100
CBell64 <72112627+CBell64@users.noreply.github.com>
2022-03-30 17:50:26 -0700

    Add files via upload

---
+++ FormController.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package controllers;
+
+/**
+ * @author ieisha
+ *
+ */
+public class FormController {
+
+}
---
+++ Order.java
@@ -0,0 +1,60 @@
+package beans;
+
+public class Order {
+	int ID = 0;
+	String orderNo = "";
+	String productName = "";
+	float price = 0;
+	int quantity = 0;
+	
+	public Order() {
+		
+	}
+	public Order(int ID, String orderNo, String productName, float price, int quantity) {
+		this.ID = ID;
+		this.orderNo = orderNo;
+		this.productName = productName;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		this.ID = iD;
+	}
+
+	public String getOrderNo() {
+		return orderNo;
+	}
+
+	public void setOrderNo(String orderNo) {
+		this.orderNo = orderNo;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+}
